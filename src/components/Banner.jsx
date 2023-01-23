0859bb6a0655db61d898ccd35d9067f39b2a2afc
Raphael Limbikani Mbewe <rmbewe68@gmail.com>
2023-01-23 10:25:55 +0200

    chore:change layout size

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -37,68 +37,66 @@ const clients = [
 
 const Banner = () => {
   return (
-    <section className="bg-[#FFFFFF]">
-      <div className="flex flex-col lg:flex-row">
-        <div className="bg-[#FAF6EA] px-6  lg:px-12 py-[80px]">
-          <div className="flex flex-col">
-            <div className="h-full w-full">
-              <Logo className="h-[123px] w-[300px] lg:w-auto" />
+    <section className="bg-[#FFFFFF] flex flex-col md:flex-row">
+      <div className="w-full md:w-1/2 bg-[#FAF6EA] px-6  md:px-[90px] py-[80px]">
+        <div className="flex flex-col">
+          <div>
+            <Logo className="w-full max-w-xs md:max-w-sm lg:max-w-md" />
+          </div>
+          <span className="font-Inter font-light lg:text-[80px]  lg:leading-[100.02px] leading-[50.02px] text-[50px]">
+            Get ready for a
+          </span>
+          <span className="font-Inter font-bold lg:text-[70px] lg:leading-[100.02px] leading-[50.02px] text-[50px] mb-2 lg:mb-0">
+            new beginning.
+          </span>
+          <p className="font-Inter font-light lg:text-[50px] lg:leading-[60.51px] leading-[40.02px] text-[40px]">
+            We’ll be here soon with our new website,subscribe to be notified.
+          </p>
+          {/* Input */}
+          <div className="relative mb-6 pt-10">
+            <div className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer h-8 lg:bottom-[-40px] my-auto">
+              <p className="font-Inter font-semibold text-[#000000] mr-2">
+                Notify Me
+              </p>
+              <svg
+                aria-hidden="true"
+                className="w-5 h-5 text-[#000000] flex-none"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+              </svg>
             </div>
-            <span className="font-Inter font-light lg:text-[80px]  lg:leading-[100.02px] leading-[50.02px] text-[50px]">
-              Get ready for a
-            </span>
-            <span className="font-Inter font-bold lg:text-[70px] lg:leading-[100.02px] leading-[50.02px] text-[50px] mb-2 lg:mb-0">
-              new beginning.
-            </span>
-            <p className="font-Inter font-light lg:text-[50px] lg:leading-[60.51px] leading-[40.02px] text-[40px]">
-              We’ll be here soon with our new website,subscribe to be notified.
-            </p>
-            {/* Input */}
-            <div className="relative mb-6 pt-10">
-              <div className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer h-8 lg:bottom-[-40px] my-auto">
-                <p className="font-Inter font-semibold text-[#000000] mr-2">
-                  Notify Me
-                </p>
-                <svg
-                  aria-hidden="true"
-                  className="w-5 h-5 text-[#000000] flex-none"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
-                  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
-                </svg>
+            <input
+              type="email"
+              id="input-group-1"
+              class="bg-[#FAF6EA] font-Inter text-sm  block w-full  p-2.5 border-x-0  border-t-0 border-b border-[#000000] focus:border-[#7bd2dd] outline-none placeholder-[#000000]"
+              placeholder="Email address*"
+            />
+          </div>
+        </div>
+        {/* clients */}
+        <div className="flex flex-col pt-20">
+          <span className="font-Inter font-normal text-[35px] leading-[42.36px]">
+            Happy clients include:
+          </span>
+          <div className="lg:flex lg:space-x-6 pt-4 grid grid-cols-3 gap-x-6">
+            {clients.map((client, index) => (
+              <div className="h-auto w-auto" key={index}>
+                <img
+                  src={client.logo}
+                  alt={client.name}
+                  className="w-full h-full"
+                />
               </div>
-              <input
-                type="email"
-                id="input-group-1"
-                class="bg-[#FAF6EA] font-Inter text-sm  block w-full  p-2.5 border-x-0  border-t-0 border-b border-[#000000] focus:border-[#7bd2dd] outline-none placeholder-[#000000]"
-                placeholder="Email address*"
-              />
-            </div>
-          </div>
-          {/* clients */}
-          <div className="flex flex-col pt-20">
-            <span className="font-Inter font-normal text-[35px] leading-[42.36px]">
-              Happy clients include:
-            </span>
-            <div className="lg:flex lg:space-x-6 pt-4 grid grid-cols-3 gap-x-6">
-              {clients.map((client, index) => (
-                <div className="h-auto w-auto" key={index}>
-                  <img
-                    src={client.logo}
-                    alt={client.name}
-                    className="w-full h-full"
-                  />
-                </div>
-              ))}
-            </div>
+            ))}
           </div>
         </div>
-        <div className="w-full h-full">
-          <img src={hero} alt="man" className="h-full w-full" />
-        </div>
+      </div>
+      <div className="w-full md:w-1/2">
+        <img src={hero} alt="man" className="h-full w-full" />
       </div>
     </section>
   );
